ad7128bfb6b51ecccd045f327706dc6b09887376
ben <b9008088@newcastle.ac.uk>
2023-04-06 12:50:15 +0000

    Initial commit with some ideas

---
+++ src/Fitness/fitness_functions.py
@@ -0,0 +1,8 @@
+# identify bounding wall
+# Check wall has no airgaps
+# Check for windows
+# Check for interior space
+# Check percentage of interior space made of air
+# Check has a door on low floor
+# Check for slanted roof? or flat roof?
+# Check for diversity
