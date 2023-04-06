f5507cc684e10f6ab44b0895a56b12e4d41489f8
ben <b9008088@newcastle.ac.uk>
2023-04-06 12:49:00 +0000

    Added modification of config file

--- src/neat.py
+++ src/neat.py
@@ -1,8 +1,14 @@
 import neat
 import numpy as np
 
-def run(config_file, fitness_function, checkpoint_path="checkpoints/",
+def run(config_file:str, fitness_function:object, checkpoint_path="checkpoints/",
         n_generations=100):
+    """
+    Performs NEAT model creation and evolution for n_generations 
+    Configuration for NEAT given by config file 
+    Models are incrementally checkpointed and saved in checkpoint_path
+    returns the best performing model
+    """
     # Load configuration.
     config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
                          neat.DefaultSpeciesSet, neat.DefaultStagnation,
@@ -27,15 +33,28 @@ def run(config_file, fitness_function, checkpoint_path="checkpoints/",
     
     return winner_net
 
-def config_inputs(path, value):
+def edit_config(path:str, input_size=None, pop_size=None,
+                       fitness_threshold=None) -> None:
+    """
+    Updates NEAT configuration file with input size given by value
+    """
     # read file
     with open(path, "r") as fs:
         lines = fs.readlines()
     # edit line
     for l in range(len(lines)):
-        if lines[l][:10] == "num_inputs":
-            lines[l] = f"num_inputs              = {value}\n"
+        if not input_size is None:
+            if lines[l][:10] == "num_inputs":
+                lines[l] = f"num_inputs              = {input_size}\n"
+        
+        if not pop_size is None:
+            if lines[l][:8] == "pop_size":
+                lines[l] = f"pop_size              = {pop_size}\n"
     
+        if not fitness_threshold is None:
+            if lines[l][:17] == "fitness_threshold":
+                lines[l] = f"fitness_threshold              = {fitness_threshold}\n"
+
     # write back lines
     with open(path, "w") as fs:
         fs.writelines(lines)
