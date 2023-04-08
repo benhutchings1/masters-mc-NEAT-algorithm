ee78751000a4a1345e81d12dd3957cb9a90ad487
Ben <bhutchings1@newcastle.ac.uk>
2023-04-08 16:52:56 +0000

    Updated run conditinons

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ from src.Blocks import block_interactions
 
 config_path = "config/NEAT-config"
 blocks_path = "blocks.csv"
-neat.edit_config(config_path, input_size = 23, pop_size=2)
+neat.edit_config(config_path, input_size = 26, pop_size=2)
 neat.run(config_path, fitness.test_fitness, n_generations=1)
