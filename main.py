ed8f18b4c2d9d3c7c42440bbd0f9125bbcf82083
Ben <bhutchings1@newcastle.ac.uk>
2023-04-13 18:15:11 +0000

    Test configuration

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from src import neat
 from src.Fitness import fitness, generate_building
 from src.Blocks import block_interactions
 from tests.structure_test import test_structure
-import numpy as np
 
 
 config_path = "config/NEAT-config"
-blocks_path = "blocks.csv"
-neat.edit_config(config_path, input_size = 26, pop_size=20)
-print(neat.run(config_path, fitness.test_fitness, n_generations=1000))
+blocks_path = "src/Blocks/blocks.csv"
+bi = block_interactions.BlockReader(block_path=blocks_path, connect=False)
+neat.edit_config(config_path, input_size = 26, pop_size=10, 
+                 output_size=len(bi.blocklist))
+print(neat.run(config_path, fitness.test_fitness, n_generations=1))
