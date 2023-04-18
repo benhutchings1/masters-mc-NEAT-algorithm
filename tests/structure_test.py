349b0d903b0e6584286eae7124efe586aa0654b6
Ben <bhutchings1@newcastle.ac.uk>
2023-04-18 17:24:03 +0000

    structural tests

--- tests/structure_test.py
+++ tests/structure_test.py
@@ -1,9 +1,15 @@
-from src.Fitness import fitness_functions
+from src.House_Fitness import fitness_functions
 from src.Blocks import block_interactions
+import numpy as np
 
 def test_structure():
     house = "tests/testdata/house.csv"
-    bi = block_interactions.BlockReader(block_path="blocks.csv", connect=False)
+    bi = block_interactions.BlockInterface(block_path="src/Blocks/blocks.csv", connect=False)
     house = bi.read_np(house)
-    print(fitness_functions.single_structure_fitness(input=[0, 0, 0, 13, 23, 23], output=house))
+    print("House Fitness")
+    for key, value in fitness_functions.single_structure_fitness(input=[0, 0, 0, 13, 23, 23], output=house).items():
+        print(f"{key}: {value}")
+    print()
+    for key, value in fitness_functions.single_structure_fitness(input=[0, 0, 0, 13, 23, 23], output=np.random.rand(8, 8, 9)).items():
+        print(f"{key}: {value}")
     
