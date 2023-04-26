08640bb24daa79a4239db8dee2a53ea75a3892ed
Ben <bhutchings1@newcastle.ac.uk>
2023-04-26 09:59:27 +0000

    Added multiple structure generation

--- builder.py
+++ builder.py
@@ -1,8 +1,9 @@
 from src.neat import Neat
 from src.Blocks import block_interactions
 import numpy as np
-from src.roof_fitness import generate_roof, fitness as roof_fit
-from src.house_fitness import generate_building, fitness as struct_fit
+from src.roof_fitness import generate_roof
+from src.house_fitness import generate_building
+import neat
 
 class CheckpointPlacer:
     def __init__(self, x, y, z, type, config_path, block_path,
@@ -15,8 +16,8 @@ class CheckpointPlacer:
         self.block_path = block_path
         self.checkpoint_path = checkpoint_path
         
-    def place_checkpoint(self, fitness, checkpoint_name, height, length, width, seeds=None):
-        self.nt = Neat(self.config_path, self.block_path, self.checkpoint_path, "temp/",fitness)
+    def place_best_from_checkpoint(self, fitness, checkpoint_name, height, length, width, seeds=None):
+        self.nt = Neat(self.config_path, self.block_path, self.checkpoint_path, "temp/",fitness, overwrite=False)
         self.nt.connect()
         model = self.nt.checkpoint_best_genome(checkpoint_name, as_model=True)
 
@@ -27,55 +28,67 @@ class CheckpointPlacer:
                 raise ValueError
             else:
                 self.house_placer(model, height, length, width, seeds)
-                
+    
+    def place_population_from_checkpoint(self, checkpoint_name, height, length, width, seeds=None):
+        self.nt = Neat(self.config_path, self.block_path, self.checkpoint_path, "temp/", None)
+        self.nt.connect()
+        population = self.nt.load_genomes_checkpoint(checkpoint_name)
+        nets = [neat.nn.FeedForwardNetwork.create(gen, self.nt.config) 
+                for __, gen in population
+        ]
+        if self.type == 1:
+            builds = [
+                self.roof_generator(net, height, width, length) for net in nets
+            ]
+        else:
+            builds = [
+                self.house_generator(net, height, width, length, seeds=seeds) for net in nets
+            ]
+        self.placer(builds, axis=0)
             
-    def roof_placer(self, model, height, length, width):
-        blocks = self.nt.convert_heightmap(
+    def roof_generator(self, model, height, length, width):
+        return self.nt.convert_heightmap(
                 generate_roof.generate(0, model, [height, length, width])[1]
             )
-        self.nt.place_blocks_np(blocks, x0=self.x, y0=self.y, z0=self.z,
-                                isblocklist=True)
         
 
-    def house_placer(self, model, height, length, width, seeds):
-        blocks = generate_building.generate(0, model,
+    def house_generator(self, model, height, length, width, seeds):
+        return generate_building.generate(0, model,
                                             [height, length, width]+seeds)[1]
-        self.nt.place_blocks_np(blocks, x0=self.x, y0=self.y, z0=self.z,
+        
+    def placer(self, structures, axis=0):
+        for struct in structures:
+            self.nt.place_blocks_np(struct, x0=self.x, y0=self.y, z0=self.z,
                                 isblocklist=True)
-    
-t = 1
-x = 25
-block_path = "src/Blocks/blocks.csv"
-bi = block_interactions.BlockInterface(block_path, connect=False)
-
-if t == 1:   
-    fit = roof_fit.Fitness(block_path, "temp", "temp")
-    cp = CheckpointPlacer(x, -50, 1, 1, "config/Roof-NEAT-config",
-        block_path,"logs/roof/checkpoints")
-    checkpoints = ["NEAT-checkpoint-4", "NEAT-checkpoint-199", "NEAT-checkpoint-644", "NEAT-checkpoint-934"]
-    for i, check in enumerate(checkpoints):
-        cp.place_checkpoint(
-            fit,
-            check,
-            2, 6, 6    
+            print(struct)
+            if axis == 0:
+                self.x += len(struct[0]) + 1
+            else:
+                self.z += len(struct)
+     
+if __name__ == "__main__":
+    tp = 1
+    if tp == 0:       
+        config = "config/Roof-NEAT-config"
+        checkpoints = "logs/roof/checkpoints"
+        cp = CheckpointPlacer(0, -50, 0, 1,
+                            config_path=config,
+                            block_path="src/Blocks/blocks.csv",
+                            checkpoint_path=checkpoints
+                            )
+        cp.place_population_from_checkpoint(
+            checkpoint_name="NEAT-checkpoint-9999",
+            height=4, length=8, width=8
         )
-        cp.z += 9
-    
-else:
-    fit = struct_fit.Fitness(block_path, "temp", "temp")
-    cp = CheckpointPlacer(x, -60, 1, 2, "config/House-NEAT-config",
-        block_path,"logs/house/checkpoints")
-
-    checkpoints = ["NEAT-checkpoint-4", "NEAT-checkpoint-24", "NEAT-checkpoint-49", "NEAT-checkpoint-69"]
-    seeds = [12, 13, 40]
-    print("Using seed blocks")
-    print(bi.blocklist[seeds[0]])
-    print(bi.blocklist[seeds[1]])
-    print(bi.blocklist[seeds[2]])
-    for i, check in enumerate(checkpoints):
-        cp.place_checkpoint(
-            fit,
-            check,
-            5, 6, 6, seeds
+    else:
+        config = "config/House-NEAT-config"
+        checkpoints = "logs/house/checkpoints"
+        cp = CheckpointPlacer(0, -60, 0, 2,
+                            config_path=config,
+                            block_path="src/Blocks/blocks.csv",
+                            checkpoint_path=checkpoints
+                            )
+        cp.place_population_from_checkpoint(
+            checkpoint_name="NEAT-checkpoint-989",
+            height=8, length=8, width=8, seeds=[12, 13, 14]
         )
-        cp.z += 9
