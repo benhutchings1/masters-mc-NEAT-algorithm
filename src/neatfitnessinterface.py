f95409ad32b0f625d4044614ebd8537729adbbdf
Ben <bhutchings1@newcastle.ac.uk>
2023-04-26 10:03:40 +0000

    Allowed for control of checkpoint rate

--- src/neatfitnessinterface.py
+++ src/neatfitnessinterface.py
@@ -4,7 +4,7 @@ from src.roof_fitness.fitness import Fitness as RoofFitness
 
 class RoofInterface(neat.Neat):
     def __init__(self, config_file:str, block_path:str, log_root:str, n_generations=1000, 
-                 n_input=None, n_pop=None, n_output=None, overwrite_logs=True):
+                 n_input=None, n_pop=None, n_output=None, overwrite_logs=True, checkpoint_rate=10):
         
         self.fit_func = RoofFitness(block_path, log_root+"novelty/", log_root+"struct/", overwrite=overwrite_logs)
         super().__init__(
@@ -17,11 +17,12 @@ class RoofInterface(neat.Neat):
             n_generations=n_generations, 
             n_input=n_input, 
             n_pop=n_pop, 
-            n_output=n_output)
+            n_output=n_output,
+            checkpoint_rate=checkpoint_rate)
 
 class HouseInterface(neat.Neat):
     def __init__(self, config_file:str, block_path:str, log_root:str, n_generations=1000, 
-                 n_input=None, n_pop=None, n_output=None, overwrite_logs=True):
+                 n_input=None, n_pop=None, n_output=None, overwrite_logs=True, checkpoint_rate=10):
         
         self.fit_func = HouseFitness(block_path, log_root+"novelty/", log_root+"struct/", overwrite=overwrite_logs)
         super().__init__(
@@ -34,5 +35,6 @@ class HouseInterface(neat.Neat):
             n_generations=n_generations, 
             n_input=n_input, 
             n_pop=n_pop, 
-            n_output=n_output)
+            n_output=n_output,
+            checkpoint_rate=checkpoint_rate)
         
