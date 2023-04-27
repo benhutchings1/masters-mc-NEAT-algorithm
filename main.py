6444575d4ed595908d7b2dc61fea391a1f56dab6
Ben <bhutchings1@newcastle.ac.uk>
2023-04-27 14:43:30 +0000

    Configed for full experiment run

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
 from src.neatfitnessinterface import RoofInterface, HouseInterface
-import numpy as np
-import os
 from src.Blocks.block_interactions import BlockInterface
+
 
 block_path = "src/Blocks/blocks.csv"
 bi = BlockInterface(block_path, connect=False)
 
-roof_net = RoofInterface(
-    config_file="config/Roof-NEAT-config",
-    block_path=block_path,
-    log_root="logs/roof/",
-    overwrite_logs=True,
-    n_generations=5000,
-    n_input=13,
-    n_output=8,
-    n_pop=5,
-).run()
+iters = [
+    ["data/control/iter1/", {"use_novelty":False}],
+    ["data/control/iter2/", {"use_novelty":False}],
+    ["data/high_novelty/iter1/", {"use_novelty":True, "novelty_ratio":(1, 4)}],
+    ["data/high_novelty/iter2/", {"use_novelty":True, "novelty_ratio":(1, 4)}],
+    ["data/low_novelty/iter1/", {"use_novelty":True, "novelty_ratio":(3, 4)}],
+    ["data/low_novelty/iter2/", {"use_novelty":True, "novelty_ratio":(3, 4)}],
+    ["data/dynamic/iter1/", {"use_novelty":True, "use_dynamic_novelty":True}],
+    ["data/dynamic/iter2/", {"use_novelty":True, "use_dynamic_novelty":True}],
+]
 
 
-# house_net = HouseInterface(
-#     config_file="config/House-NEAT-config",
-#     block_path=block_path,
-#     log_root="logs/house/",
-#     overwrite_logs=True,
-#     n_generations=1000,
-#     n_input=26,
-#     n_output=len(bi.blocklist)-1,
-#     n_pop=5,
-# ).run()
+for path, par in iters:
+    print(par)    
+    roof_net = RoofInterface(
+        config_file="config/Roof-NEAT-config",
+        block_path=block_path,
+        log_root=path+"roof/",
+        overwrite_logs=True,
+        n_generations=1,
+        n_input=13,
+        n_output=8,
+        n_pop=100,
+        checkpoint_rate=5,
+        **par
+    ).run()
+
+    house_net = HouseInterface(
+        config_file="config/House-NEAT-config",
+        block_path=block_path,
+        log_root=path+"house/",
+        overwrite_logs=True,
+        n_generations=1,
+        n_input=26,
+        n_output=len(bi.blocklist)-1,
+        n_pop=10,
+        checkpoint_rate=5,
+        **par
+    ).run()
+
