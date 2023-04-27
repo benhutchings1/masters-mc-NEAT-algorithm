2c5f3c30aaaf91e16148748e71347268c01137eb
Ben <bhutchings1@newcastle.ac.uk>
2023-04-27 14:38:39 +0000

    Implemented switchin between different novelty methods

--- src/roof_fitness/fitness.py
+++ src/roof_fitness/fitness.py
@@ -7,13 +7,34 @@ import random
 import math
 
 class Fitness:
-    def __init__(self, block_path, novelty_log_path, struct_log_path, overwrite=True) -> None:
+    def __init__(self, block_path, novelty_log_path, struct_log_path, overwrite=True, use_novelty=True, use_dynamic_novelty=False, novelty_ratio=None, squash_function=True) -> None:
         # Setup novelty calculator with logging
-        self.novelty = nvlty.Novelty(novelty_log_path, overwrite=overwrite)
+        self.novelty = nvlty.Novelty(novelty_log_path, overwrite=overwrite, squash_function=squash_function)
         # Setup block interactor
         self.bi = block_interactions.BlockInterface(block_path=block_path, connect=False)
         # Setup structural data logger
         self.struct_logger = logger.StructLogger(struct_log_path, "struct_log.csv", overwrite_log=overwrite)
+        
+        #  Default to no novelty (control)
+        self.nov_tech = 0
+        
+        if use_novelty:
+            # Dynamic novelty or fixed novelty
+            if use_dynamic_novelty:
+                # Save novelty type
+                self.nov_tech = 1
+                # Init dynamic novelty object
+                self.dn = nvlty.DynamicNovelty()
+            else:
+                # Check if novelty ratio is defined
+                if novelty_ratio is None:
+                    raise ValueError("No novelty technique specified")
+                # Get novelty ratio
+                assert type(novelty_ratio) == tuple and len(novelty_ratio) == 2
+                self.r_nov = novelty_ratio[0]
+                self.r_score = novelty_ratio[1]
+                # Save novelty type
+                self.nov_tech = 2
         
     def __call__(self, genomes:list, config:neat.Config, return_best=False):
         """
@@ -69,20 +90,27 @@ class Fitness:
 
     def fitness_test(self, genomes:neat.DefaultGenome, input:np.array, outputs:np.array) -> int:
         """
-        Gets fitness of each genome
+        Gets fitness for each genome
         """
-        # Start logging generation
+        # Start new logging generation
         self.struct_logger.start_gen()
         
-        # Compute ratio of structure score to novelty
-        r_nov, r_score = 1, 2 # r_nov:r_score
-        p_nov = r_nov/(r_nov+r_score)
-        p_score = r_score/(r_nov+r_score)
+        novelty_scores = self.novelty.novelty_score(genomes, math.ceil(len(genomes)/2))
+        struct_scores = structure_functions.structure_score(input, outputs, logger=self.struct_logger) 
         
-        # Compute structure score and novelty then combine to make fitness score
-        out = []
-        novelty = self.novelty.novelty_score(genomes, math.ceil(len(genomes)/2))
-        score = structure_functions.structure_score(input, outputs, logger=self.struct_logger) 
-        for ni, sc in zip(novelty, score):
-            out.append(ni * p_nov + sc * p_score)
-        return out
+        # No novelty (control)
+        if self.nov_tech == 0:
+            return struct_scores
+        else:
+            if self.nov_tech == 1:  
+                return self.dn.get_ratios(novelty_scores, struct_scores)   
+            else:
+                # STATIC ratio
+                # Compute percentage of novelty:structure scores
+                p_nov = self.r_nov / (self.r_nov + self.r_score)
+                p_score = self.r_score / (self.r_nov + self.r_score)
+                # Take percentages of each score as fitness
+                fitness = []
+                for ns, ss in zip(novelty_scores, struct_scores):
+                    fitness.append(ns * p_nov + ss * p_score)
+                return fitness
