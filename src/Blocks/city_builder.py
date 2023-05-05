f5d2302fce0126b8f57a9df5967e5237fb9f1cbf
Ben <bhutchings1@newcastle.ac.uk>
2023-05-05 16:02:23 +0000

    Impemented building of cities and orientation placement

--- src/Blocks/city_builder.py
+++ src/Blocks/city_builder.py
@@ -1,13 +1,20 @@
 from . import block_interactions
 from src.house_fitness import generate_building
 from src.roof_fitness import generate_roof
-from src import neat
+from src import neat as my_neat
+import neat
 import random
 
 class CityBuilder(block_interactions.BlockInterface):
-    def __init__(self, block_path=None, mutate_chance=0.1):
-        super().__init__(block_path, connect=False)
+    def __init__(self, block_path, house_config, roof_config, mutate_chance=0.1):
+        super().__init__(block_path, connect=True)
         self.block_path = block_path
+        self.house_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                            neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                            house_config)
+        self.roof_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                            neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                            roof_config)
         self.house_pop = []
         self.roof_pop = []
         self.WIDTH = 6
@@ -20,7 +27,7 @@ class CityBuilder(block_interactions.BlockInterface):
         
         
     def read_in_pop(self, config_path, checkpoint_path, checkpoint_name, house=True):
-        nt = neat.Neat(config_file=config_path, checkpoint_path=checkpoint_path, overwrite=False)
+        nt = my_neat.Neat(config_file=config_path, checkpoint_path=checkpoint_path, overwrite=False)
         cp = nt.load_checkpoint(checkpoint_name)
         for i in list(cp.population.items()):
             if house:
@@ -34,11 +41,9 @@ class CityBuilder(block_interactions.BlockInterface):
 
         if len(self.roof_pop) == 0:
             raise ValueError("Roof population cannot be empty")
-        # Initial building specs
-        
         
         # Initial position
-        pos = {"x":x0, "z":z0}
+        pos = {"x0":x0, "z0":z0}
         # Get direction to move in
         first_dir, second_dir = self.__conv_orientation(orientation)
         
@@ -49,40 +54,61 @@ class CityBuilder(block_interactions.BlockInterface):
             for __ in range(it+1):
                 # Place at temp position
                 # Random chance to mutate
-                
+                self.mutate_params()
+                # Choose random models
+                house_model = neat.nn.FeedForwardNetwork.create(random.choice(self.house_pop), self.house_config)
+                roof_model = neat.nn.FeedForwardNetwork.create(random.choice(self.roof_pop), self.roof_config)                                
                 # Generate roof and house
+                house = generate_building.generate(0, house_model, 
+                                                   [self.HOUSE_HEIGHT, self.LENGTH, self.WIDTH]+self.seeds)[1]
+                roof = generate_roof.generate(0, roof_model, [self.ROOF_HEIGHT, self.LENGTH, self.WIDTH])[1]
                 
+                # Place generated building
+                self.place_house(house, self.WIDTH, self.LENGTH, y0=y0, orientation=orientation,
+                                 isblocklist=True, **temp_position)
+                self.place_roof(roof, orientation=orientation, y0=y0+self.HOUSE_HEIGHT, **temp_position)
                 
                 # Update temp position
-                temp_position[first_dir[0]] -= first_dir[1] * plot_size
-                temp_position[second_dir[0]] += second_dir[1] * plot_size
+                temp_position[first_dir[0]] -= (first_dir[1] * plot_size) + gap
+                temp_position[second_dir[0]] += (second_dir[1] * plot_size) + gap
+            
             # After all placed update position
             pos[first_dir[0]] += first_dir[1]*plot_size                 
     
     def __conv_orientation(self, orientation):
         if orientation == "N":
-            return [["z", 1],["x", 1]]
+            return [["z0", -1],["x0", 1]]
         elif orientation == "E":
-            return [["x", 1],["z", -1]]
+            return [["x0", 1],["z0", 1]]
         elif orientation == "S":
-            return [["z", -1],["x", -1]]
+            return [["z0", 1],["x0", -1]]
         elif orientation == "W":
-            return [["x", -1],["z", 1]]
+            return [["x0", -1],["z0", -1]]
 
-    def __mutate(self):
-        values = [self.WIDTH, self.LENGTH, self.HOUSE_HEIGHT, self.ROOF_HEIGHT]
-        lims = [[5, 15],[5, 15],[2, 7],[2, 5]]
-        chances = random.sample(range(0, 100), 5)
-        
-        for val, chan, lim in zip(values, chances, lims):
-            if chan < 100 * (self.mutate_chance/2):
-                if val - 1 >= lims[0]:
-                    val -= 1
-            
-            if chan > 100 - (100 * (self.mutate_chance/2)):
-                if val + 1 <= lims[1]:
-                    val += 1
-        
-        ## DO SEED MUTATE AND TEST THIS
+    def mutate_params(self):
+        self.WIDTH = self.mutate(self.WIDTH, [5, 10])
+        self.LENGTH = self.mutate(self.LENGTH, [5, 10])
+        self.HOUSE_HEIGHT = self.mutate(self.HOUSE_HEIGHT, [2, 7])
+        self.ROOF_HEIGHT = self.mutate(self.ROOF_HEIGHT, [2, 5])
+        self.seeds[0] = self.mutate(self.seeds[0], [1, len(self.blocklist) - 1], add=False) 
+        self.seeds[1] = self.mutate(self.seeds[1], [1, len(self.blocklist) - 1], add=False)
+        self.seeds[2] = self.mutate(self.seeds[2], [1, len(self.blocklist) - 1], add=False)
     
-    
+    def mutate(self, value, lim, add=True):
+        rnd = random.randint(0, 100)
+        
+        if rnd <= (100 * self.mutate_chance):
+            if add:
+                if not value - 1 < lim[0]:
+                    value -= 1
+            else:
+                value = random.randint(lim[0], lim[1])
+                
+        elif rnd >= 100 - (100 * self.mutate_chance):
+            if add:
+                if not value + 1 > lim[1]:
+                    value += 1
+            else:
+                value = random.randint(lim[0], lim[1])
+        
+        return value
