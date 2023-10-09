aec1e0c94087350a2ded7d810300f57dc8cd98fe
Ubuntu <azureuser@vmbentest.cnig4xnfoxdu3cksfmgxzemw5g.ax.internal.cloudapp.net>
2023-10-09 11:16:09 +0000

    Migrated config options in individual files to singular location

---
+++ CONFIG.py
@@ -0,0 +1,8 @@
+BLOCK_PATH = "src/Blocks/blocks.csv"            # Location of block data
+SERVER_CONNECT = False                          # Should program attempt connection to MC server
+DATA_OUTPUT_PATH = "./data"                     # Output path of generated data
+TEST_DATA_PATH="./testdata"                     # Location of sample test data
+VISUALISATION_OUTPUT = "./vis_output"           # Output of visualisation graphs
+HOUSE_CONFIG_PATH="config/House-NEAT-config"    # Location of Config file (Structure Model)
+ROOF_CONFIG_PATH="config/Roof-NEAT-config"      # Location of Config file (Roof Model)
+CITY_GENERATION_SIZE = 5                        # Size of city to generate
