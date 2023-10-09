203504586bea10cae2a9d86a21db841403c7b3cf
Ubuntu <azureuser@vmbentest.cnig4xnfoxdu3cksfmgxzemw5g.ax.internal.cloudapp.net>
2023-10-09 12:08:27 +0000

    Improved commenting and added config options

--- run_full_training.py
+++ run_full_training.py
@@ -1,20 +1,31 @@
 from src.neatfitnessinterface import RoofInterface, HouseInterface
 from src.Blocks.block_interactions import BlockInterface
-from typing import List
-import neat
-import random
-import numpy as np
-from src.house_fitness import structure_functions
-from src.house_fitness import generate_building
+import CONFIG
 
-# Output location for generated data
-out = "data"
+# ============== EXPERIMENT CONFIGURATION ============== 
+# Model config
+CONFIG_ROOF_EXP = {                             # Roof Model
+    "config_file": "config/Roof-NEAT-config",   # NEAT roof model config file (default: config/Roof-NEAT-config)
+    "n_generations": 5,                         # Number of generations to run roof model
+    "n_output": 8,                              # The max roof height for the building
+    "n_pop": 50,                                # Model population size
+    "checkpoint_rate": 1                        # Make a population checkpoint (for recovery and testing) every n generations
+}
+CONFIG_STRUCT_EXP = {                           # Structure Model
+    "config_file": "config/House-NEAT-config",  # NEAT structure model config file (default: config/House-NEAT-config)
+    "n_generations": 5,                         # Number of generations to run structure model
+    "n_pop": 10,                                # Model population size
+    "checkpoint_rate": 1                        # Make a population checkpoint (for recovery and testing) every n generations
+}
 
-# Establish block connection
-block_path = "src/Blocks/blocks.csv"
-bi = BlockInterface(block_path, connect=True)
-
-# Experiment configs
+# Experiment specific configs
+# 3 experiment types, each run for 2 iterations
+# Experiment types:
+#       Control (no novelty)
+#       Low Novelty 
+#       High Novelty
+# Output location for generated data retrieved from CONFIG.py
+out = CONFIG.DATA_OUTPUT_PATH
 iters = [
     [f"{out}/control/iter1/", {"use_novelty":False}],
     [f"{out}/control/iter2/", {"use_novelty":False}],
@@ -24,47 +35,65 @@ iters = [
     [f"{out}/low_novelty/iter2/", {"use_novelty":True, "novelty_ratio":(4, 1)}],
 ]
 
+# ============== RUN EXPERIMENTS ============== 
+# Define block interface
+bi = BlockInterface(CONFIG.BLOCK_PATH, connect=CONFIG.SERVER_CONNECT)
 # Iterate over all experimentation parameters
 for i, (path, parameters) in enumerate(iters):
+    print()
+    print("=================================================")
     print(f"Experiment {i}")
-    print("Training roof model")
+    print(f"Training roof model for experiment {i}")
     roof_net = RoofInterface(
         # Config file path
-        config_file="config/Roof-NEAT-config",
+        config_file=CONFIG_ROOF_EXP["config_file"],
         # Path to block descriptors
-        block_path=block_path,
+        block_path=CONFIG.BLOCK_PATH,
         # Path to log files root
         log_root=path+"roof/",
         # Choose to overwrite logs instead of appending
         overwrite_logs=True,
         # Training parameters
-        n_generations=1,
+        n_generations=CONFIG_ROOF_EXP["n_generations"],
         n_input=13,
-        n_output=8, # Max roof height 
-        n_pop=100,
+        n_output=CONFIG_ROOF_EXP["n_output"], 
+        n_pop=CONFIG_ROOF_EXP["n_pop"],
         # How often checkpoints are taken
-        checkpoint_rate=5,
+        checkpoint_rate=CONFIG_ROOF_EXP["checkpoint_rate"],
         # Experimentation specific parameters
         **parameters
     ).run()
-    print("Training house model")
+    # To restart training from an existing checkpoint use
+    # .run_from_checkpoint(path_to_checkpoint)
+    # instead of .run()
+    
+    print()
+    print("=================================================")
+    print()
+    
+    print(f"Training house model for experiment {i}")
     house_net = HouseInterface(
         # Config file path
-        config_file="config/House-NEAT-config",
+        config_file=CONFIG_STRUCT_EXP["config_file"],
         # Path to block descriptors
-        block_path=block_path,
+        block_path=CONFIG.BLOCK_PATH,
         # Path to log files root
         log_root=path+"house/",
         # Choose to overwrite logs instead of appending
         overwrite_logs=True,
         # Training parameters
-        n_generations=1,
+        n_generations=CONFIG_STRUCT_EXP["n_generations"],
         n_input=16,
-        n_pop=3,
+        n_pop=CONFIG_STRUCT_EXP["n_pop"],
         # Model output size fixed to no. blocks
         n_output=len(bi.blocklist)-1,
         # How often checkpoints are taken
-        checkpoint_rate=5,
+        checkpoint_rate=CONFIG_STRUCT_EXP["checkpoint_rate"],
         # Experimentation specific parameters
         **parameters
     ).run()
+    # To restart training from an existing checkpoint use
+    # .run_from_checkpoint(path_to_checkpoint)
+    # instead of .run()
+    print("=================================================")
+    print()
