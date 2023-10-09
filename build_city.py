bfefa02f324e3884cccbc80696464f2d2f2a1598
Ubuntu <azureuser@vmbentest.cnig4xnfoxdu3cksfmgxzemw5g.ax.internal.cloudapp.net>
2023-10-09 12:04:13 +0000

    Added automatic checkpoint discovery + comments

--- build_city.py
+++ build_city.py
@@ -1,53 +1,72 @@
 from src.Blocks.city_builder import CityBuilder
-from src.Blocks.block_interactions import BlockInterface
-import numpy as np
-from src.house_fitness.structure_functions import score_block_variation
-import random
 from src.visualise import multi_struct_plot
+import CONFIG
 import os
-import skimage.morphology as morph  
-from src.Blocks import no_comm_gen
 
-house_config = "config/House-NEAT-config"
-roof_config = "config/Roof-NEAT-config"
-block_path = "src/Blocks/blocks.csv"
+city_data_out = CONFIG.VISUALISATION_OUTPUT + "/city"
+
+# Automatically get highest trained models
+def get_models(data_root):
+    def get_biggest_dir(path):
+        # Get version number
+        files = [int(file[16:]) for file in os.listdir(path)]
+        # Get highest version
+        return max(files)
+    out = {}
+    for src in ["control", "high_novelty", "low_novelty"]:
+        out[src] = [["1", "1"],["1", "1"]]
+        for n_iter, iter in enumerate(["iter1", "iter2"]):
+            for n_mod, mod in enumerate(["house", "roof"]):
+                # Format path
+                loc = f"{CONFIG.DATA_OUTPUT_PATH}/{src}/{iter}/{mod}/checkpoints"
+                # Save biggest
+                out[src][n_iter][n_mod] = str(get_biggest_dir(loc))
+    return out
+
+# Automatically get model version
+run_config = get_models(CONFIG.DATA_OUTPUT_PATH)
+
+# Specify which generation of model to use
+# Update depending on how far into training got
+# Will be automatically found but can be overriden by changing if statement
+if False:
+    run_config = {
+        # model type: [[house_iter1, roof_iter1],[house_iter2, roof_iter2]]
+        "high_novelty":[["1", "1"],["1", "1"]],
+        "control":[["1", "1"],["1", "1"]],
+        "low_novelty":[["1", "1"],["1", "1"]]
+    }
+
 
 builders = {}
-
-run_config = {
-    "high_novelty":[["22974", "22974"],["21979", "2999"]],
-    "all_novelty":[["2999", "2999"],["2999", "2999"]],
-    "control":[["22974", "22974"],["21979", "21979"]],
-    "low_novelty":[["22974", "22974"],["21979", "21979"]]
-}
-
-
-for source in ["high_novelty", "all_novelty", "control", "low_novelty"]:
+# Get all models from saved models
+for source in ["high_novelty", "control", "low_novelty"]:
     checkpoints = run_config[source]
     houses = [
-        [f"data/{source}/iter1/house/checkpoints/", f"NEAT-checkpoint-{checkpoints[0][0]}"],
-        [f"data/{source}/iter2/house/checkpoints/", f"NEAT-checkpoint-{checkpoints[0][1]}"]
+        [f"{CONFIG.DATA_OUTPUT_PATH}/{source}/iter1/house/checkpoints/", f"NEAT-checkpoint-{checkpoints[0][0]}"],
+        [f"{CONFIG.DATA_OUTPUT_PATH}/{source}/iter2/house/checkpoints/", f"NEAT-checkpoint-{checkpoints[0][1]}"]
     ]
     roofs = [
-        [f"data/{source}/iter1/roof/checkpoints", f"NEAT-checkpoint-{checkpoints[1][0]}"],
-        [f"data/{source}/iter2/roof/checkpoints", f"NEAT-checkpoint-{checkpoints[1][1]}"]
+        [f"{CONFIG.DATA_OUTPUT_PATH}/{source}/iter1/roof/checkpoints", f"NEAT-checkpoint-{checkpoints[1][0]}"],
+        [f"{CONFIG.DATA_OUTPUT_PATH}/{source}/iter2/roof/checkpoints", f"NEAT-checkpoint-{checkpoints[1][1]}"]
     ]
 
 
-    cb = CityBuilder(block_path="src/Blocks/blocks.csv", house_config=house_config, roof_config=roof_config, 
-                    log_path="temp/", filename="multilog.txt", overwrite_log=True, connect=False)
+    cb = CityBuilder(block_path=CONFIG.BLOCK_PATH, house_config=CONFIG.HOUSE_CONFIG_PATH, roof_config=CONFIG.ROOF_CONFIG_PATH, 
+                    log_path=city_data_out, filename="multilog.txt", overwrite_log=True, connect=CONFIG.SERVER_CONNECT)
     builders[source] = cb
 
     # Read in populations
     for (h_path, h_check),(r_path, r_check) in zip(houses, roofs): 
-        cb.read_in_pop(config_path=house_config,checkpoint_path=h_path, checkpoint_name=h_check)
-        cb.read_in_pop(config_path=roof_config, checkpoint_path=r_path, checkpoint_name=r_check, house=False)
+        cb.read_in_pop(config_path=CONFIG.HOUSE_CONFIG_PATH,checkpoint_path=h_path, checkpoint_name=h_check)
+        cb.read_in_pop(config_path=CONFIG.ROOF_CONFIG_PATH, checkpoint_path=r_path, checkpoint_name=r_check, house=False)
 
-    cb.place_city(iterations=200, plot_size=15, orientation="N", x0=0, z0=0, gap=2)
+    print(f"==== Using {source} models ====")
+    cb.place_city(iterations=CONFIG.CITY_GENERATION_SIZE, plot_size=15, orientation="N", x0=1, z0=1, gap=2)
 
 # Results analysis
 pl = multi_struct_plot.MultiStructurePlot()
-pl.plot_bar_var(builders, "out/", "variance_comparison.png")
-pl.plot_gen_time_bar(builders, "out/", "times.png")
-pl.plot_block_frequencies(builders, "out/", "block_freq.png")
-pl.plot_all_scores(builders, "out/", ["house_scores.png", "roof_scores.png"])
+pl.plot_bar_var(builders, city_data_out+"/", "variance_comparison.png")
+pl.plot_gen_time_bar(builders, city_data_out+"/", "times.png")
+pl.plot_block_frequencies(builders, city_data_out+"/", "block_freq.png")
+pl.plot_all_scores(builders, city_data_out+"/", ["house_scores.png", "roof_scores.png"])    
